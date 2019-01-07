b950658893c6412e2b44e948d892e08e41855ee5
Manuel Meraz <meraz.manuel@gmail.com>
2019-01-06 16:08:39 -0800

    modified tests to handle food package

--- tests/context.py
+++ tests/context.py
@@ -4,4 +4,4 @@ import sys
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../src')))
 
-import food_utils
+import food
--- tests/test_food_utils.py
+++ tests/test_food_utils.py
@@ -2,35 +2,35 @@
 
 import unittest
 import os
-from context import food_utils
-from food_utils import Food, Macronutrients, Serving
+from context import food
+from food import Food, Macronutrients, Serving, utils
 
 
 class TestFoodUtils(unittest.TestCase):
     def setUp(self):
-        food_utils.create_food_db()
+        utils.create_food_db()
 
     def tearDown(self):
-        food_utils.delete_food_db()
+        utils.delete_food_db()
 
     def test_insert_and_get_food(self):
         serving = Serving('g', 100)
         macros = Macronutrients(100, 1, 1, 1, 1)
-        food = Food("food", macros, serving)
-        food_utils.insert_food(food)
+        new_food = Food("food", macros, serving)
+        utils.insert_food(new_food)
 
-        foods = food_utils.get_food("food")
+        foods = utils.get_food("food")
         found_food = False
 
-        for food in foods:
-            if food.name == "food":
+        for f in foods:
+            if f.name == "food":
                 found_food = True
 
                 break
         self.assertTrue(found_food)
 
     def test_get_food_not_in_db(self):
-        self.assertEqual(len(food_utils.get_food("food")), 0)
+        self.assertEqual(len(utils.get_food("food")), 0)
 
     def test_serving_first_arg(self):
         self.assertRaises(AttributeError, Serving, 100, 100)
@@ -47,11 +47,11 @@ class TestFoodUtils(unittest.TestCase):
 
 class TestFoodUtilsDBFunctions(unittest.TestCase):
     def test_create_food_db(self):
-        food_utils.create_food_db()
+        utils.create_food_db()
         self.assertTrue('food.db' in os.listdir('.'))
 
     def test_remove_food_db(self):
-        food_utils.delete_food_db()
+        utils.delete_food_db()
         self.assertFalse('food.db' in os.listdir('.'))
 
 
