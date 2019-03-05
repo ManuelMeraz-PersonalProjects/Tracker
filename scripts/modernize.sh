c2f268e781ed31ec62d97948b6bd502550174d6f
Manuel Meraz <meraz.manuel@gmail.com>
2019-03-04 21:54:32 -0800

    simple clang tidy script implemented, really need to fix

---
+++ scripts/modernize.sh
@@ -0,0 +1,43 @@
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-override' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-avoid-bind' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-deprecated-headers' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-loop-convert' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-make-shared' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-make-unique' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-pass-by-value' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-raw-string-literal' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-redundant-void-arg' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-replace-auto-ptr' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-shrink-to-fit' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-auto' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-bool-literals' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+ ./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-default-member-init' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-emplace' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-equals-default' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-equals-delete' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-nullptr' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-override' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-transparent-functors' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+./run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-using' -fix -p ../build -clang-apply-replacements-binary `which clang-apply-replacements-7`
+
+
