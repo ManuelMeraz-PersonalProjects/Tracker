918ba45955143bd61cc53e8a8a6b245d36e0e2b7
Manuel Meraz <meraz.manuel@gmail.com>
2019-03-09 17:05:10 -0800

    added comments to configure script

--- scripts/configure_and_make.sh
+++ scripts/configure_and_make.sh
@@ -1,4 +1,7 @@
 # You can run this script from anywhere and it will configure and build your code
+
+# TRACKER_PROJECT contains the root directory of the tracker poject
+# if TRACKER_PROJECT not set, then
 if [ -z "${TRACKER_PROJECT}" ]; then
 	cat <<-EOS
 	Please run the following command from the root directory
@@ -10,13 +13,19 @@ if [ -z "${TRACKER_PROJECT}" ]; then
 	exit 1
 fi
 
+# Build directory does not exist
 if [ ! -d ${TRACKER_PROJECT}/build ]; then
 	mkdir ${TRACKER_PROJECT}/build
+# CMakeCache keeps previous build configuration, which can conflict with 
+# development when modifying CMakeLists.txt
+# Removing this removes any old configuration
 elif [ -e ${TRACKER_PROJECT}/build/CMakeCache.txt ]; then
 	rm ${TRACKER_PROJECT}/build/CMakeCache.txt 
 fi
 
+# If cding into build fails, exit the script
 cd ${TRACKER_PROJECT}/build || exit
+
 # Set CMAKE settings here
 cmake \
 -DCMAKE_BUILD_TYPE=Debug \
