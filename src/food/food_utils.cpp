f99b9b5953ea0bcd8cd5e1c65a3f247d9a310b6e
Manuel Meraz <meraz.manuel@gmail.com>
2019-03-09 16:27:29 -0800

    main now linked to soci and put into build/bin

--- src/food/food_utils.cpp
+++
@@ -1,5 +0,0 @@
-#include <food/food_utils.h>
-
-int food_utils::sum(int n1, int n2) {
-	return n1 + n2;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
-#include <food/food_utils.h>
+#include "soci.h"
+#include "soci-sqlite3.h"
+#include <exception>
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+
+using namespace soci;
+using namespace std;
+
+bool get_name(string &name) {
+  cout << "Enter name: ";
+	cin >> name;
+  return true;
+}
 
 int main() {
-	std::cout << food_utils::sum(5, 5) << std::endl;
+  try {
+    session sql("sqlite3", "db=mydb.sqlite timeout=2 shared_cache=true");
+
+    int count;
+    sql << "select count(*) from phonebook", into(count);
+
+    cout << "We have " << count << " entries in the phonebook.\n";
+
+    string name;
+    while (get_name(name)) {
+      string phone;
+      indicator ind;
+      sql << "select phone from phonebook where name = :name", into(phone, ind),
+          use(name);
+
+      if (ind == i_ok) {
+        cout << "The phone number is " << phone << '\n';
+      } else {
+        cout << "There is no phone for " << name << '\n';
+      }
+    }
+  } catch (exception const &e) {
+    cerr << "Error: " << e.what() << '\n';
+  }
 }
