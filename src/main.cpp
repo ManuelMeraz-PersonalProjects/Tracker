2d7b13f71ac6fd6d9d0c817f2dd19659eeda72d8
Manuel Meraz <meraz.manuel@gmail.com>
2019-03-09 17:39:51 -0800

    sqlite3 and soci now work, need to create table

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,9 @@
-#include "soci.h"
-#include "soci-sqlite3.h"
-#include <exception>
-#include <iostream>
-#include <istream>
-#include <ostream>
-#include <string>
-
-using namespace soci;
-using namespace std;
-
-bool get_name(string &name) {
-  cout << "Enter name: ";
-	cin >> name;
-  return true;
-}
+#include <soci-sqlite3.h>
+#include <soci.h>
 
 int main() {
-  try {
-    session sql("sqlite3", "db=mydb.sqlite timeout=2 shared_cache=true");
-
-    int count;
-    sql << "select count(*) from phonebook", into(count);
-
-    cout << "We have " << count << " entries in the phonebook.\n";
-
-    string name;
-    while (get_name(name)) {
-      string phone;
-      indicator ind;
-      sql << "select phone from phonebook where name = :name", into(phone, ind),
-          use(name);
-
-      if (ind == i_ok) {
-        cout << "The phone number is " << phone << '\n';
-      } else {
-        cout << "There is no phone for " << name << '\n';
-      }
-    }
-  } catch (exception const &e) {
-    cerr << "Error: " << e.what() << '\n';
-  }
+  soci::session db("sqlite3", "db=test.db timeout=2 shared_cache=true");
+  int i = 7;
+  int j = 8;
+  db << "insert into table(x, y) values(?, ?)", soci::use(i), soci::use(j);
 }
