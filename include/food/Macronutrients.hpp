59e023785b18a560a0cc3c842298ff7897c57adb
Manuel Meraz <meraz.manuel@gmail.com>
2019-03-12 20:20:12 -0700

    added @brief to a bunch of classes, forgot to do that

--- include/food/Macronutrients.hpp
+++ include/food/Macronutrients.hpp
@@ -11,22 +11,22 @@
 #define FOOD_MACRONUTRIENTS_HPP
 
 /**
- * Stores the fat content of a food
+ * @brief Stores the fat content of a food
  */
 struct Fat {
   /**
    * @param f The fat content of the food
    */
-  explicit Fat(double f) : fat{f}{};
+  explicit Fat(double f) : fat{f} {};
 
   /**
-   *  The fat content of the food
+   *  @brief The fat content of the food
    */
   const double fat;
 };
 
 /**
- * Stores the fiber content of a food
+ * @brief Stores the fiber content of a food
  */
 struct Fiber {
   /**
@@ -35,61 +35,64 @@ struct Fiber {
   explicit Fiber(double f) : fiber{f} {}
 
   /**
-   * The total fiber content of the food
+   * @brief The total fiber content of the food
    */
   const double fiber;
 };
 
 /**
- * Stores the carbohydrate content of a food
+ * @brief Stores the carbohydrate content of a food
  */
 struct Carbohydrate {
   /**
    * @param c The total carbohydrate content of the food
    */
-  explicit Carbohydrate(double tc) : total_carb(tc), fiber{0}{};
+  explicit Carbohydrate(double tc) : total_carb(tc), fiber{0} {};
 
   /**
    * @param tc The total carbohydrate content of the food
    * @param f The fiber content of the food
    */
-  explicit Carbohydrate(double tc, const Fiber &f) : total_carb{tc}, fiber{f}{};
+  explicit Carbohydrate(double tc, const Fiber &f)
+      : total_carb{tc}, fiber{f} {};
 
   /**
-   *  The total carb content of the food
+   * @brief The total carb content of the food
    */
   const double total_carb;
 
   /**
-   *  The fiber content of the food
+   *  @brief The fiber content of the food
    */
   const Fiber fiber;
 };
 
+/**
+ * @brief Stores the protein content of a food
+ */
 struct Protein {
   /**
-   * Stores the protein content of a food
    * @param p The protein content of the food
    */
-  explicit Protein(double p) : protein{p}{};
+  explicit Protein(double p) : protein{p} {};
 
   /**
-   *  The protein content of the food
+   *  @brief The protein content of the food
    */
   const double protein;
 };
 
 /**
- * The macronutrients class stores all macronutrient data to be stored in a Food
- * object
+ * @brief The macronutrients class stores all macronutrient data to be stored in
+ *a Food object
  */
 class Macronutrients {
 public:
   Macronutrients() : fat{0}, carb{0}, protein{0} {}
 
   /**
-   * The classes passed in to this class are strongly typed classes
-   * to help illustrate the data being passed in.
+   * @brief The classes passed in to this class are strongly typed classes
+   *				to help illustrate the data being passed in.
    *
    * @param f The fat content of the food
    * @param c The carbohydrate content of the food. Pass by value.
@@ -99,13 +102,13 @@ public:
       : fat{f}, carb{c}, protein{p} {}
 
   /**
-   * Copy constructor for lvalues reference
+   * @brief Copy constructor for lvalues reference
    * @param macros The macros to be copied
    */
   Macronutrients(const Macronutrients &macros) = default;
 
   /**
-   * Move constructor for rvalue reference
+   * @brief Move constructor for rvalue reference
    * @param macros The macros to be moved
    */
   Macronutrients(Macronutrients &&macros) = default;
@@ -117,13 +120,15 @@ public:
   Macronutrients &operator=(const Macronutrients &macros) = delete;
 
   /**
-   * Move assignment operator
+   * @brief Move assignment operator
    * @param macros The macros to be moved
    */
   Macronutrients &operator=(Macronutrients &&macros) = delete;
 
   /**
-   * All data will be retrieved from a storable object using this function.
+   * @brief All data will be retrieved from a storable object using this
+   *				function.
+	 *
    * @return This A pair containing the column where the data will be store
    *				 and the data itself.
    */
@@ -134,17 +139,17 @@ public:
 
 private:
   /**
-   *  The fat content of the food
+   *  @brief The fat content of the food
    */
   const Fat fat;
 
   /**
-   *  The carbohydrate content of the food
+   *  @brief The carbohydrate content of the food
    */
   const Carbohydrate carb;
 
   /**
-   *  The protein content of the food
+   *  @brief The protein content of the food
    */
   const Protein protein;
 };
