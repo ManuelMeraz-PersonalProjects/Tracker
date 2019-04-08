96c32a354c0eddfd6b660d7e68ecef8b826a241f
Manuel Meraz <meraz.manuel@gmail.com>
2019-04-08 14:37:19 -0700

    cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,19 @@
 #include "database/utils.hpp"
 #include "food/Food.hpp"           // Food
 #include "food/Macronutrients.hpp" // Fat, Carbohydrate, Protein
-#include <nameof.hpp>
-#include <iostream>
 #include <string_view>
 
-enum class test { A, B };
-
 int main() {
-   food::Macronutrients macros(food::Fat(10),
-   food::Carbohydrate(10, food::Fiber(10)),
-   food::Protein(10));
+  food::Macronutrients macros(food::Fat(10),
+                              food::Carbohydrate(10, food::Fiber(10)),
+                              food::Protein(10));
 
-   food::Food taco(macros, "tacos");
-   database::utils::insert(taco);
+  food::Food taco(macros, "tacos");
+  database::utils::insert(taco);
 
-   const auto entries = database::utils::retrieve<food::Food>();
-   for(const auto& entry:entries) {
-     std::cout << entry << std::endl;
-   }
-   std::cout << "filler: " << NAMEOF_ENUM(test::A) << " :filler" << std::endl;
+  auto entries = database::utils::retrieve<food::Food>();
+  for (const std::string_view &entry : entries) {
+    std::cout << entry << std::endl;
+  }
   return 0;
 }
