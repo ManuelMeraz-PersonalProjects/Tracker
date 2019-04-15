b9045d8cf5327b8c3eb2656a508dd86ba1fb9384
Manuel Meraz <meraz.manuel@gmail.com>
2019-04-15 10:17:46 -0700

    done with main features of utils

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,27 @@ namespace utils = database::utils;
 
 auto main() -> int
 {
+  auto &all_food = utils::retrieve_all<Food>();
   std::cout << "Creating food..." << std::endl;
   for (size_t i = 0; i < 100; ++i) {
     Macronutrients macros(Fat(i), Carbohydrate(i, Fiber(i)), Protein(i));
     utils::make<Food>("tacos", macros);
   }
 
-  auto &all_food = utils::retrieve_all<Food>();
+  std::cout << "Confirm creation in tracker.db" << std::endl;
+  std::cout << "Press enter to continue." << std::endl;
+  std::cin.ignore();
+
+  std::cout << "Modifying food..." << std::endl;
+  for (size_t i = 0; i < 100; ++i) {
+    Macronutrients macros(Fat(i*i), Carbohydrate(i*i, Fiber(i*i)), Protein(i*i));
+    all_food[i].set_macronutrients(macros);
+  }
+
+  std::cout << "Confirm updates in tracker.db" << std::endl;
+  std::cout << "Press enter to continue." << std::endl;
+  std::cin.ignore();
+
   std::cout << "Deleting..." << std::endl;
   while (!all_food.empty()) {
     utils::delete_storable(all_food.back());
