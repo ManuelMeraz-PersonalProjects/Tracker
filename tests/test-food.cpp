a4089c220db639654e0575660a04b32df39f02d9
Manuel Meraz <meraz.manuel@gmail.com>
2019-04-15 21:36:33 -0700

    simple google test

--- tests/test-food.cpp
+++ tests/test-food.cpp
@@ -1,4 +1,11 @@
-int main() {
-	int success = 0;
-	return success;
+#include "gtest/gtest.h"
+
+TEST(TestSuite, Test) {
+  EXPECT_EQ(true, true);
+}
+
+auto main(int argc, char **argv) -> int
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
