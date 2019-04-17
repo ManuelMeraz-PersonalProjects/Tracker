dd3315a378d67101290c9c57d968ca5a4689156e
Manuel Meraz <meraz.manuel@gmail.com>
2019-04-17 13:34:09 -0700

    ignoring anything with build in it's name

--- scripts/configure_and_make.sh
+++ scripts/configure_and_make.sh
@@ -1,5 +1,13 @@
 # You can run this script from anywhere and it will configure and build your code
 
+# If an argument is supplied, then the build directory
+# name is set to that argument
+if [ ! -z "$1" ]; then
+  build_dir="$1"
+else
+  build_dir="build"
+fi
+
 # TRACKER_PROJECT contains the root directory of the tracker poject
 # if TRACKER_PROJECT not set, then
 if [ -z "${TRACKER_PROJECT}" ]; then
@@ -14,17 +22,17 @@ if [ -z "${TRACKER_PROJECT}" ]; then
 fi
 
 # Build directory does not exist
-if [ ! -d ${TRACKER_PROJECT}/build ]; then
-	mkdir ${TRACKER_PROJECT}/build
+if [ ! -d ${TRACKER_PROJECT}/$build_dir ]; then
+	mkdir ${TRACKER_PROJECT}/$build_dir
 # CMakeCache keeps previous build configuration, which can conflict with 
 # development when modifying CMakeLists.txt
 # Removing this removes any old configuration
-elif [ -e ${TRACKER_PROJECT}/build/CMakeCache.txt ]; then
-	rm ${TRACKER_PROJECT}/build/CMakeCache.txt 
+elif [ -e ${TRACKER_PROJECT}/$build_dir/CMakeCache.txt ]; then
+	rm ${TRACKER_PROJECT}/$build_dir/CMakeCache.txt 
 fi
 
 # If cding into build fails, exit the script
-cd ${TRACKER_PROJECT}/build || exit
+cd ${TRACKER_PROJECT}/$build_dir || exit
 
 # Set CMAKE settings here
 cmake \
