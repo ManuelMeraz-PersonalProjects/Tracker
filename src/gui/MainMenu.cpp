85ee760bf7239810bdb88b1451a71f6438cef026
Manuel Meraz <meraz.manuel@gmail.com>
2019-05-21 20:44:45 -0700

    ctrl q now quits the program

--- src/gui/MainMenu.cpp
+++ src/gui/MainMenu.cpp
@@ -1,21 +1,30 @@
 #include "MainMenu.hpp"
 
+#include <QApplication>
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QAction>
 
 MainMenu::MainMenu(QWidget *parent) : QWidget(parent)
 {
 
-  QVBoxLayout *vbox = new QVBoxLayout(this);
+  auto *vbox = new QVBoxLayout(this);
   vbox->setSpacing(1);
 
-  QPushButton *food_database = new QPushButton("Food Database", this);
-  food_database->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-  QPushButton *quit = new QPushButton("Quit", this);
-  quit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  QPushButton *food_db_button = new QPushButton("Food Database", this);
+  food_db_button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-  vbox->addWidget(food_database);
-  vbox->addWidget(quit);
+  auto *quit_button = new QPushButton("Quit", this);
+  quit_button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+  auto *quit = new QAction("Quit", this);
+  quit->setShortcut(tr("CTRL+Q"));
+  quit_button->addAction(quit);
+  connect(quit_button, &QPushButton::clicked, qApp, &QApplication::quit);
+  connect(quit, &QAction::triggered, qApp, &QApplication::quit);
+
+  vbox->addWidget(food_db_button);
+  vbox->addWidget(quit_button);
 
   setLayout(vbox);
 }
